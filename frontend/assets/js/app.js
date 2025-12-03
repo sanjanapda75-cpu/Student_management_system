c2d808de2d3734c0bed407bb71fcbe6abebc9034
sanjanapda75-cpu <sanjana.pda75@gmail.com>
2025-12-03 06:17:53 +0000

    new folder frontend created

--- frontend/assets/js/app.js
+++
@@ -1,9 +0,0 @@
-// Main entrypoint for frontend
-import { initStudentController } from "./controllers/studentController.js";
-import { router } from "./router/viewRouter.js";
-
-// Initialize app on page load
-window.addEventListener("DOMContentLoaded", () => {
-  router();
-  initStudentController();
-});
